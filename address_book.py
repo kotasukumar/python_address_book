41ec24090b20716f33e57c3254508501180a96ea
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-08-11 14:29:50 +0530

    modified

--- address_book.py
+++ address_book.py
@@ -15,14 +15,25 @@ class Contact:
                         self.email_id, self.mobile_number]
 
 
-if __name__ == "__main__":
-    print("Welcome to address book")
-    contact_list = []
-    while True:
-        user_input = int(
-            input("Enter your choice\n1:Add the contact\n2:Edit the contact\n3:Delete the contact\n4:Display"
-                  "\n5:Exit: "))
-        if user_input == 1:
+def add():
+    f_name = input("Enter first name: ")
+    l_name = input("Enter last name: ")
+    address = input("Enter the address: ")
+    city = input("Enter the city: ")
+    state = input("Enter the state: ")
+    pincode = input("Enter the pin code: ")
+    email = input("Enter email id: ")
+    mobile_number = input("Enter mobile number: ")
+    result = Contact(f_name, l_name, address, city, state, pincode, email, mobile_number)
+    contact_list.append(result.details)
+
+
+def edit():
+    f_name = input("Enter first name: ")
+    l_name = input("Enter last name: ")
+    for i in contact_list:
+        if f_name == i[0] and l_name == i[1]:
+            print("Contact found, please enter details to update")
             f_name = input("Enter first name: ")
             l_name = input("Enter last name: ")
             address = input("Enter the address: ")
@@ -31,32 +42,31 @@ if __name__ == "__main__":
             pincode = input("Enter the pin code: ")
             email = input("Enter email id: ")
             mobile_number = input("Enter mobile number: ")
-            result = Contact(f_name, l_name, address, city, state, pincode, email, mobile_number)
-            contact_list.append(result.details)
-            print(contact_list)
+            index = contact_list.index(i)
+            contact_list[index] = [f_name, l_name, address, city, state, pincode, email, mobile_number]
+
+
+if __name__ == "__main__":
+    print("Welcome to address book")
+    contact_list = []
+    while True:
+        user_input = int(
+            input("Enter your choice\n1:Add the contact\n2:Edit the contact\n3:Delete the contact\n4:Display"
+                  "\n5:Exit: "))
+        if user_input == 1:
+            add()
+            print("Contact added successfully")
         elif user_input == 2:
-            f_name = input("Enter first name: ")
-            l_name = input("Enter last name: ")
-            for i in contact_list:
-                if f_name == i[0] and l_name == i[1]:
-                    print("Contact found, please enter details to update")
-                    f_name = input("Enter first name: ")
-                    l_name = input("Enter last name: ")
-                    address = input("Enter the address: ")
-                    city = input("Enter the city: ")
-                    state = input("Enter the state: ")
-                    pincode = input("Enter the pin code: ")
-                    email = input("Enter email id: ")
-                    mobile_number = input("Enter mobile number: ")
-                    index = contact_list.index(i)
-                    contact_list[index] = [f_name, l_name, address, city, state, pincode, email, mobile_number]
-            print(contact_list)
+            edit()
+            print("Contact edited successfully")
         elif user_input == 3:
             f_name = input("Enter first name: ")
             l_name = input("Enter last name: ")
             for i in contact_list:
                 if f_name == i[0] and l_name == i[1]:
                     contact_list.remove(i)
+            print("Contact deleted successfully")
+        elif user_input == 4:
             print(contact_list)
         elif user_input == 5:
             sys.exit()
