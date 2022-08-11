806a65993f09dd408e84867490855262f80ca7dc
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-08-11 13:01:21 +0530

    added parameters

--- address_book.py
+++ address_book.py
@@ -1,1 +1,18 @@
-print("Welcome to address book")
+class Contact:
+    def __init__(self, first_name, last_name, address, city, state, pin_code, email_id, mobile_number):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.city = city
+        self.state = state
+        self.pin_code = pin_code
+        self.email_id = email_id
+        self.mobile_number = mobile_number
+
+        print(self.first_name, self.last_name, self.address, self.city, self.state, self.pin_code, self.email_id, self.mobile_number)
+
+
+if __name__ == "__main__":
+    print("Welcome to address book")
+    Contact('sukumar', 'kota', 'nimandhakara', 'chittoor', 'AP', '51701', 'sukumar@gmail.com', '7563614102')
+
