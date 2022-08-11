e3b89372c55d038af6a443e78b8a2eb2f7e20e6c
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-08-11 13:23:31 +0530

    adding conatact

--- address_book.py
+++ address_book.py
@@ -9,10 +9,23 @@ class Contact:
         self.email_id = email_id
         self.mobile_number = mobile_number
 
-        print(self.first_name, self.last_name, self.address, self.city, self.state, self.pin_code, self.email_id, self.mobile_number)
+        details = [self.first_name, self.last_name, self.address, self.city, self.state, self.pin_code,
+                   self.email_id, self.mobile_number]
+
+        print(details)
 
 
 if __name__ == "__main__":
     print("Welcome to address book")
-    Contact('sukumar', 'kota', 'nimandhakara', 'chittoor', 'AP', '51701', 'sukumar@gmail.com', '7563614102')
-
+    user_input = int(input("Enter your choice\n1:Add the contact\n2:Edit the contact\n3:Delete the contact\n4:display"
+                           "\n5:exit: "))
+    if user_input == 1:
+        f_name = input("Enter first name: ")
+        l_name = input("Enter last name: ")
+        address = input("Enter the address: ")
+        city = input("Enter the city: ")
+        state = input("Enter the state: ")
+        pincode = input("Enter the pin code: ")
+        email = input("Enter email id: ")
+        mobile_number = input("Enter mobile number: ")
+        Contact(f_name, l_name, address, city, state, pincode, email, mobile_number)
